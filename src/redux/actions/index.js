7bb8059fc6192b2a4255d5be04881b33e79994ed
Adão Benites <adaobjr@gmail.com>
2021-08-23 20:23:44 -0400

    feat: ✨ created Actions

---
+++ src/redux/actions/index.js
@@ -0,0 +1,75 @@
+// import getCurrencies from '../api';
+
+import fetchAPI from '../../services';
+
+// LOGIN ------------------------------------------------------------------------------------------
+export const ADD_LOGIN = 'ADD_LOGIN';
+
+export const addLogin = (email, password) => ({
+  type: ADD_LOGIN,
+  payload: {
+    email,
+    password,
+  },
+});
+
+// PRODUCTS ---------------------------------------------------------------------------------------
+
+const SAVE_PRODUCT_LIST = 'SAVE_PRODUCT_LIST';
+export const saveProductList = (listShoes) => ({
+  type: SAVE_PRODUCT_LIST, payload: { listShoes },
+});
+
+// Fetch Thunk
+
+// Thunk com Async / Await
+// export function fetchShoes() {
+//   return async (dispatch) => {
+//     const listShoes = await fetchAPI();
+//     try {
+//       return dispatch(saveProductList(listShoes.result));
+//     } catch (error) {
+//       return dispatch(saveProductList(error.message));
+//     }
+//   };
+// }
+
+// Thunk com Then
+export const fetchShoes = () => (dispatch) => {
+  fetchAPI()
+    .then((shoesListSuccess) => dispatch(
+      saveProductList(shoesListSuccess.result),
+    ))
+    .catch((error) => dispatch(saveProductList(error.message)));
+};
+
+// -------------------------------------------------------------------------------------------------
+
+export const SET_FAVORITE = 'SET_FAVORITE'; // ACTION -> SET_FAVORITE
+export const setFav = (favorited) => ({ // ACTION-CREATOR -> SET_FAVORITE
+  type: SET_FAVORITE, payload: { favorited },
+});
+
+export const ADD_CART = 'ADD_CART'; // ACTION -> ADD_PRODUCTS
+export const addCart = (cart) => ({ // ACTION-CREATOR -> ADD_PRODUCTS
+  type: ADD_CART, payload: { cart },
+});
+
+export const ADD_TOTAL_CART = 'ADD_TOTAL_CART'; // ACTION -> ADD_TOTAL_CART
+export const addTotalCart = (totalCart) => ({ // ACTION-CREATOR -> ADD_TOTAL_CART
+  type: ADD_TOTAL_CART, payload: { totalCart },
+});
+
+export const SET_FETCHON_DONE = 'SET_FETCHON_DONE'; // ACTION -> SET_FETCHON_DONE
+export const setFetchOnDone = (fetchOn, done) => ({ // ACTION-CREATOR -> SET_FETCHON_DONE
+  type: SET_FETCHON_DONE, payload: { fetchOn, done },
+});
+
+export const SET_DONE_LOADING = 'SET_DONE_LOADING'; // ACTION -> SET_DONE_LOADING
+export const setDoneLoading = (done, loading) => ({ // ACTION-CREATOR -> SET_DONE_LOADING
+  type: SET_DONE_LOADING, payload: { done, loading },
+});
+
+export const SET_SCREEN_HOME = 'SET_SCREEN_HOME'; // ACTION -> SET_SCREEN_HOME
+export const SET_SCREEN_FAV = 'SET_SCREEN_FAV'; // ACTION -> SET_SCREEN_FAV
+export const SET_SCREEN_CART = 'SET_SCREEN_CART'; // ACTION -> SET_SCREEN_CART
