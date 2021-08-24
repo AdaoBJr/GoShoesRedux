1627a8825f73fdc457bd994a6e309829ea211762
Adão Benites <adaobjr@gmail.com>
2021-08-23 21:11:36 -0400

    feat: ✨ created getProducts / useDispatch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addProduct } from './redux/actions';
+import { fetchAPI2 } from './services';
 
 function App() {
+  const dispatch = useDispatch();
+  const getProducts = async () => {
+    // const response = await fetchAPI().get('products');
+    const response = await fetchAPI2();
+    // const allProducts = response.products;
+    const allProducts = response.results;
+    dispatch(addProduct(allProducts));
+  };
+
+  // CICLOS DE VIDA
+  useEffect(getProducts, []);
+
+  // ---------------------------------------------------------------------------------------------
+
   return (
     <div>
       Iniciando
