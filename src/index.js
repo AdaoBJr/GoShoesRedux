568bb2f900955e655e5733a0b44fe2a677b0a512
Adão Benites <adaobjr@gmail.com>
2021-08-24 08:01:50 -0400

    feat: :sparkles: added HashRouter in Index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>,
   document.getElementById('root'),
 );
