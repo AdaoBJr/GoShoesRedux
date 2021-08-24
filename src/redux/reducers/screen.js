c9fe472dbb5cc22166bb4dff53ee90a8e8d5ef11
Adão Benites <adaobjr@gmail.com>
2021-08-23 20:23:05 -0400

    feat: ✨ created screenReducer

---
+++ src/redux/reducers/screen.js
@@ -0,0 +1,55 @@
+import {
+  SET_SCREEN_HOME, SET_SCREEN_FAV, SET_SCREEN_CART, SET_FETCHON_DONE, SET_DONE_LOADING,
+} from '../store';
+
+const screenReducer = (state, { type, payload }) => { // Desestruturação do Action
+  switch (type) {
+    case SET_FETCHON_DONE: {
+      const { fetchOn, done } = payload;
+      return {
+        ...state,
+        fetchOn,
+        done,
+      };
+    }
+    case SET_DONE_LOADING: {
+      const { done, loading } = payload;
+      return {
+        ...state,
+        loading,
+        done,
+      };
+    }
+    case SET_SCREEN_HOME: {
+      return {
+        ...state,
+        home: true,
+        colec: false,
+        fav: false,
+        carT: false,
+      };
+    }
+    case SET_SCREEN_FAV: {
+      return {
+        ...state,
+        home: false,
+        colec: false,
+        fav: true,
+        carT: false,
+      };
+    }
+    case SET_SCREEN_CART: {
+      return {
+        ...state,
+        home: false,
+        colec: false,
+        fav: false,
+        carT: true,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default screenReducer;
