878fc61e9fc8a28ce7ad24e4eff10b0be85a16b4
Adão Benites <adaobjr@gmail.com>
2021-08-24 06:23:20 -0400

    feat: ✨ updated Services

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,7 @@
 export const CALCADOS_URL = 'https://api.mercadolibre.com/sites/MLB/search?category=categoryId&q=tenis';
 
 const getAPI = async () => {
-  try {
-    const response = await fetch(CALCADOS_URL);
-    return response.json();
-  } catch (error) {
-    return console.log(error);
-  }
+  const response = await fetch(CALCADOS_URL);
+  return response.json();
 };
 export default getAPI;
