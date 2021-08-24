029933ea1c7fe317809bfb13446620fad5374ec3
Adão Benites <adaobjr@gmail.com>
2021-08-23 23:17:00 -0400

    feat: ✨ updated getProducts in App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { addProduct } from './redux/actions';
-import { fetchAPI2 } from './services';
+import { getProducts } from './redux/actions';
 
 function App() {
   const dispatch = useDispatch();
-  const getProducts = async () => {
-    // const response = await fetchAPI().get('products');
-    const response = await fetchAPI2();
-    // const allProducts = response.products;
-    const allProducts = response.results;
-    dispatch(addProduct(allProducts));
-  };
 
   // CICLOS DE VIDA
-  useEffect(getProducts, []);
+  useEffect(() => { dispatch(getProducts()); }, []);
 
   // ---------------------------------------------------------------------------------------------
 
