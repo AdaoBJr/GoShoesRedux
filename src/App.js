cfd1014a54d1b76bb35a2ae60301871ccf73cff4
Adão Benites <adaobjr@gmail.com>
2021-08-24 06:53:54 -0400

    feat: ✨ added Routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getProducts } from './redux/actions';
+import React from 'react';
+// import { useDispatch } from 'react-redux';
+// import { getProducts } from './redux/actions';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './common/pages/Home';
+import Login from './common/pages/Login';
+import Profile from './common/pages/Profile';
+import Favorited from './common/pages/Favorited';
+import Details from './common/pages/Details';
+import Cart from './common/pages/Cart';
+import CreditCard from './common/pages/CreditCard';
+import Purchased from './common/pages/Purchased';
 
 function App() {
-  const dispatch = useDispatch();
-
-  // CICLOS DE VIDA
-  useEffect(() => { dispatch(getProducts()); }, []);
-
-  // ---------------------------------------------------------------------------------------------
-
   return (
-    <div>
-      Iniciando
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/favorited" component={Favorited} />
+      <Route exact path="/details/:id" component={Details} />
+      <Route exact path="/cart" component={Cart} />
+      <Route exact path="/credit" component={CreditCard} />
+      <Route exact path="/purchased" component={Purchased} />
+    </Switch>
   );
 }
 
