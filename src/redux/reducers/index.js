bc5b58f0f5b3092b927d9fe3d92e0b0ba4e9a9b4
Adão Benites <adaobjr@gmail.com>
2021-08-24 21:41:09 -0400

    feat: ✨ added user State in Store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import products from './products';
 import screen from './screen';
 import cart from './cart';
+import user from './user';
 
 const reducers = combineReducers({
   products,
   screen,
   cart,
+  user,
 });
 
 export default reducers;
