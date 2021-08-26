a463449253d6df6b499a7474467518c27a7584be
Adão Benites <adaobjr@gmail.com>
2021-08-25 23:56:39 -0400

    feat: ⚡ updated GoLoginDash Component

--- src/common/components/GoLoginDash.jsx
+++ src/common/components/GoLoginDash.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Lottie from 'react-lottie';
 
 import * as GoLoginDashboard from '../../GoLoginDash.json';
+import * as GoLogoutDashboard from '../../GoLogoutDash.json';
 
-const dashboarAnimation = {
+const LoginDashboard = {
   loop: true,
   autoplay: true,
   animationData: GoLoginDashboard.default,
@@ -12,12 +14,38 @@ const dashboarAnimation = {
   },
 };
 
-export default function GoLoginDash() {
+const LogoutDashboard = {
+  loop: true,
+  autoplay: true,
+  animationData: GoLogoutDashboard.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+export default function GoLoginDash({ logout }) {
+  if (logout) {
+    return (
+      <div className="goLoginDashboard">
+        <Lottie
+          options={LogoutDashboard}
+        />
+      </div>
+    );
+  }
   return (
     <div className="goLoginDashboard">
       <Lottie
-        options={dashboarAnimation}
+        options={LoginDashboard}
       />
     </div>
   );
 }
+
+GoLoginDash.propTypes = {
+  logout: PropTypes.bool,
+};
+
+GoLoginDash.defaultProps = {
+  logout: undefined,
+};
