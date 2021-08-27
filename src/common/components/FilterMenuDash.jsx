03d3a24e7ba08f3c139ae470d597418058f9b0a8
Adão Benites <adaobjr@gmail.com>
2021-08-26 22:55:35 -0400

    feat: ✨ added animation filterMenuDash in Home Page

---
+++ src/common/components/FilterMenuDash.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+
+import * as FilterMenuDashboard from '../../filterMenu.json';
+
+const FilterDashboard = {
+  loop: true,
+  autoplay: true,
+  animationData: FilterMenuDashboard.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+export default function FilterMenuDash() {
+  return (
+    <div className="filterMenuDash">
+      <Lottie
+        options={FilterDashboard}
+      />
+    </div>
+  );
+}
