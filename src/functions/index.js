1975b350f17fd1eff700fac72ff4ed4b1a92155e
Adão Benites <adaobjr@gmail.com>
2021-08-27 11:05:47 -0400

    feat: ✨ added function setShip in Functions folder

--- src/functions/index.js
+++ src/functions/index.js
@@ -124,3 +124,26 @@ export const showQty = (id, cart) => {
   const qty = cart.reduce((acc, currValue) => acc + currValue.count, 0);
   return qty;
 };
+
+// ----------------------------------------------------------------------------------------------
+// REDUCERS
+
+// SCREEN
+// SET FILTER SHIPPING
+export const setShip = (state, filterON) => {
+  const { lowFilter, highFilter } = state;
+
+  if (!filterON) {
+    const filterState = [lowFilter, highFilter];
+    const filterOn = (filterState.filter((item) => item)[0] || false);
+    const shipFilter = !state.shipFilter;
+    return {
+      filterOn,
+      shipFilter,
+    };
+  }
+  return {
+    filterOn: filterON,
+    shipFilter: !state.shipFilter,
+  };
+};
