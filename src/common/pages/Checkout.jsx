f22b4e4aa6ddf694adca11022408381a671e2ea3
Adão Benites <adaobjr@gmail.com>
2021-08-28 08:42:49 -0400

    feat: ✨ added content in Checkout Page

--- src/common/pages/Checkout.jsx
+++ src/common/pages/Checkout.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import { FaShoppingBag } from 'react-icons/fa';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import CreditCard from '../components/CreditCard';
 
 export default function Checkout() {
-  const renderCheckout = () => {
+  const renderCheckout = () => (
+    <>
+      <div className="titleLogout">
+        <h1 className="goLoginTitle">Cadastre os dados do seu cartão de crédito</h1>
 
-  };
+      </div>
+
+      <div className="cartLogin">
+        <CreditCard />
+        <Link to="/purchased">
+          <button
+            type="button"
+            className="shopBtn"
+            // onClick={() => dispatch(setFetchOnDone(true))}
+          >
+            <FaShoppingBag className="shopIcon" />
+            Pagamento
+          </button>
+        </Link>
+      </div>
+    </>
+  );
   return (
     <>
       <Header />
