d0a8fda81cd2ed08244d6c82c879afb85beaf317
Adão Benites <adaobjr@gmail.com>
2021-08-28 02:25:03 -0400

    feat: :sparkles: added BtnsAddRem / BtnFavorited in Favorited Page

--- src/common/pages/Favorited.jsx
+++ src/common/pages/Favorited.jsx
@@ -1,24 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ALink from 'react-anchor-link-smooth-scroll';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-import {
-  FaHeart, FaRegHeart, FaMinus, FaPlus, FaShoppingCart,
-} from 'react-icons/fa';
 import { BiUpArrowAlt } from 'react-icons/bi';
-import { addCart, setFav } from '../../redux/actions';
-import { CarT, Fav, showQty } from '../../functions';
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import BtnsAddRem from '../components/BtnsAddRem';
+import BtnFavorited from '../components/BtnFavorited';
 
 export default function Favorited() {
-  const dispatch = useDispatch();
   const [ScrollY, setScrollY] = useState(false);
   const {
     products: { favorited },
-    cart: { cart },
   } = useSelector((state) => state);
 
   /*= =================== SHOW SCROLL TOP ==================== */
@@ -51,7 +46,6 @@ export default function Favorited() {
           const {
             id, title, thumbnail, availableQuantity, price,
           } = product;
-          const Qty = showQty(id, cart);
 
           return (
             <div data-aos="fade-down" data-aos-delay={200 + index * 300} className="favContent" key={id}>
@@ -65,37 +59,8 @@ export default function Favorited() {
                 {`R$ ${price
                   .toLocaleString('pt-br', { minimumFractionDigits: 2 })}`}
               </span>
-              <div className="addRemoveButtons">
-                <div
-                  aria-hidden
-                  className={(Qty > 0) ? 'removeButton' : 'opacity'}
-                  onClick={() => dispatch(addCart(CarT(product, cart, false)))}
-                >
-                  <FaMinus />
-                </div>
-                <div
-                  aria-hidden
-                  className={(Qty === 0) ? 'cartItems' : 'cartItemsN1'}
-                  onClick={() => dispatch(addCart(CarT(product, cart, true)))}
-                >
-                  <FaShoppingCart />
-                  <div className="numberItems">{ Qty }</div>
-                </div>
-                <div
-                  aria-hidden
-                  className={(Qty > 0) ? 'addButton' : 'opacity'}
-                  onClick={() => dispatch(addCart(CarT(product, cart, true)))}
-                >
-                  <FaPlus />
-                </div>
-              </div>
-              <div
-                aria-hidden
-                className="button favoritedButton"
-                onClick={() => dispatch(setFav(Fav(product, favorited)))}
-              >
-                {(favorited.find((fav) => fav.id === id)) ? <FaHeart /> : <FaRegHeart /> }
-              </div>
+              <BtnsAddRem product={product} />
+              <BtnFavorited product={product} />
             </div>
           );
         })}
