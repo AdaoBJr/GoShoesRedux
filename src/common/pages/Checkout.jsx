f6f4bb058f02b683f5918c908415b4293352ec27
Adão Benites <adaobjr@gmail.com>
2021-08-28 12:03:22 -0400

    style: :lipstick: added styles in Checkout Page

--- src/common/pages/Checkout.jsx
+++ src/common/pages/Checkout.jsx
@@ -5,17 +5,19 @@ import { FaShoppingBag } from 'react-icons/fa';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import CreditCard from '../components/CreditCard';
+import CreditCardDash from '../components/CreditCardDash';
 
 export default function Checkout() {
   const renderCheckout = () => (
     <>
       <h2 className="sectionTitle">Cadastre os dados do seu cartão de crédito</h2>
-      <div className="cartLogin">
+      <div className="checkoutContent">
+        <CreditCardDash />
         <CreditCard />
         <Link to="/purchased">
           <button
             type="button"
-            className="shopBtn"
+            className="buyBtn"
             // onClick={() => dispatch(setFetchOnDone(true))}
           >
             <FaShoppingBag className="shopIcon" />
