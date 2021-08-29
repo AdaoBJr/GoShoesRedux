6e7735a0a141385eded371be9757a89dc3880cca
Adão Benites <adaobjr@gmail.com>
2021-08-29 11:06:15 -0400

    feat: ✨ created Header.test.js

---
+++ src/tests/Header.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import '@testing-library/jest-dom';
+// import { screen } from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import Header from '../common/components/Header';
+
+import renderWithRouter from '../services/renderWithRouter';
+
+const initialState = {
+  screen: {
+    home: true,
+    fav: false,
+    carT: false,
+    profile: false,
+    lightTheme: true,
+  },
+  cart: { cart: [] },
+  user: { logIn: false },
+};
+
+describe('Testando toda a aplicação do Componente Header', () => {
+  const mockStore = configureStore();
+  let store;
+
+  it('Verifica se o título aparece no Header', () => {
+    store = mockStore(initialState);
+    const { getByText } = renderWithRouter(<Provider store={store}><Header /></Provider>);
+
+    const title = getByText(/GoShoes/i);
+    expect(title).toBeInTheDocument();
+  });
+});
